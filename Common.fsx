7bb57aff338dbe5643f0f65c8edc4e82b21e75d0
Preston TW <preston@commure.com>
2020-11-28 12:45:44 -0600

    Add digits function

--- Common.fsx
+++ Common.fsx
@@ -2,3 +2,6 @@ let manhattanDistance (x, y) = abs x + abs y
 
 let keysToSet m =
     m |> Map.toSeq |> Seq.map fst |> Set.ofSeq
+
+let digits (i: int) =
+    i |> string |> Seq.map (fun i -> int i - int '0')
