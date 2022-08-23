b4c782f47ccd56b84c898a1847abcbbab9acc3df
Preston TW <preston_tunnell_wilson@brown.edu>
2022-08-22 23:53:31 -0400

    Add tests
    and format

--- Day1.fsx
+++ Day1.fsx
@@ -1,16 +1,30 @@
 #load "Inputs.fsx"
+#r "nuget: Expecto"
+
+open Expecto
 
 let parse (input: string) = input.Split '\n' |> Seq.map int
 
 let part1 input =
     let list = parse input
+
     for i in list do
         for j in list do
             if i + j = 2020 then printf "%d" (i * j)
 
 let part2 input =
     let list = parse input
+
     for i in list do
         for j in list do
             for k in list do
-                if i + j + k = 2020 then printf "%d\n" (i * j * k)
+                if i + j + k = 2020 then
+                    printf "%d\n" (i * j * k)
+
+let tests =
+    test "A simple test" {
+        let subject = "Hello Wrorld"
+        Expect.equal subject "Hello World" "The strings should equal"
+    }
+
+let main = runTestsWithCLIArgs [] [||] tests
